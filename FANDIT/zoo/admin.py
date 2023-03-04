f278e4c6203b87102322ac4ca7134e68357523d6
Carlo Ghizzardi <carlo.ghizzardi.85@gmail.com>
2023-03-04 19:52:27 +0100

    cambios en el admin del Zoo

--- FANDIT/zoo/admin.py
+++ FANDIT/zoo/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
 from zoo.models import Zoo, AnimalRegistration
 from animal.models import Animal
+from family.models import Family
 
 
 
 
 
-#@admin.register(Zoo)
+class AnimalRegistrationInline(admin.TabularInline):
+    model = AnimalRegistration
+    
+ 
+
+class ZooAdmin(admin.ModelAdmin):
+    inlines=[AnimalRegistrationInline]
+    list_display = ['name', 'city', 'country']
+
+
+admin.site.register(Zoo, ZooAdmin)
 
 
