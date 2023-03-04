be42eab5699976b5e8af30a5f1a365febcfd6912
Carlo Ghizzardi <carlo.ghizzardi.85@gmail.com>
2023-03-04 14:06:48 +0100

    Modificaciones a los modelos

--- FANDIT/animal/api/serializers.py
+++ FANDIT/animal/api/serializers.py
@@ -5,4 +5,4 @@ from animal.models import Animal
 class AnimalSerializer(serializers.ModelSerializer):
     class Meta:
         model= Animal
-        fields= ['common_name', 'scientific_name', 'family_id', 'in_extinction', 'zoo_id']
+        fields= ['common_name', 'scientific_name', 'family_id', 'in_extinction']
--- FANDIT/animal/models.py
+++ FANDIT/animal/models.py
@@ -9,8 +9,7 @@ class Animal(models.Model):
     scientific_name= models.CharField(max_length= 100, null= False)
     family_id= models.ForeignKey(Family, on_delete= models.CASCADE, null=False)
     in_extinction= models.BooleanField(default= False, null= False)
-    zoo_id= models.ManyToManyField(Zoo)
-
+    
     def __str__(self):
         return self.common_name
 
