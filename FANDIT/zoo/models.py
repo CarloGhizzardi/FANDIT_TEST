abc82b77a055083c264f5532c9a443cb3db5e609
Carlo Ghizzardi <carlo.ghizzardi.85@gmail.com>
2023-03-04 06:44:30 +0100

    Creacion del modelo Zoo y configuracion en admin

--- FANDIT/zoo/models.py
+++ FANDIT/zoo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Zoo(models.Model):
+    name= models.CharField(max_length= 100)
+    city= models.CharField(max_length= 100)
+    country= models.CharField(max_length= 100)
+    surface= models.DecimalField(max_digits=10, decimal_places= 2)
+    budget= models.DecimalField(max_digits=20, decimal_places= 2)
+
+
+    def __str__(self):
+        return self.name
+
+
+
