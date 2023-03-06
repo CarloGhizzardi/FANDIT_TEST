22234a24b21bdff639c584939eadf56ab4548c33
Carlo Ghizzardi <carlo.ghizzardi.85@gmail.com>
2023-03-06 14:38:52 +0100

    renmbrado de tablas

--- FANDIT/zoo/models.py
+++ FANDIT/zoo/models.py
@@ -9,7 +9,7 @@ class Zoo(models.Model):
     country= models.CharField(max_length= 100)
     surface= models.DecimalField(max_digits=10, decimal_places= 2)
     budget= models.DecimalField(max_digits=20, decimal_places= 2)
-    animlal= models.ManyToManyField(Animal)
+    animal= models.ManyToManyField(Animal)
     
     
     def __str__(self):
